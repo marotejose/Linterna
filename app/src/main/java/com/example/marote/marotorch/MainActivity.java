6d62bf97c6278b552606cb16754aba55ef6950ec
marotejose <josemaiorini@gmail.com>
2016-08-18 14:30:23 -0300

    Version Inicial

--- app/src/main/java/com/example/marote/marotorch/MainActivity.java
+++ app/src/main/java/com/example/marote/marotorch/MainActivity.java
@@ -1,13 +1,37 @@
 package com.example.marote.marotorch;
 
+import android.hardware.camera2.CameraDevice;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
 
+    private CameraDevice mCam;
+
+
+
+
+
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState)
+    {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mCam = CameraDevice.
+
+        // Encender la linterna
     }
+
+    @Override
+
+    public void onBackPressed()
+        {
+            super.onBackPressed();
+
+            //Apagar la Linterna
+
+        }
+
+
 }
